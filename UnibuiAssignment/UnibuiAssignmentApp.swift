8a4880e25fc67604f424c3d9fba07fdd83ea728b
Winsome Tang <tangwins@sheridancollege.ca>
2025-01-20 21:07:38 -0500

    Initial Commit

---
+++ UnibuiAssignment/UnibuiAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnibuiAssignmentApp.swift
+//  UnibuiAssignment
+//
+//  Created by Winsome Tang on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct UnibuiAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
